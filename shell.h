b3082c8ce47efb76608e594a81c424623a2f7452
David-Mark10 <63518217+David-Mark10@users.noreply.github.com>
2023-11-16 15:05:38 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -62,7 +62,7 @@ typedef struct liststr
  *@linecount_flag: if on count this line of input
  *@fname: the program filename
  *@env: linked list local copy of environ
- *@environ: custom modified copy of environ from LL env
+ *@enviromt: custom modified copy of environ from LL env
  *@history: the history node
  *@alias: the alias node
  *@env_changed: on if environ was changed
